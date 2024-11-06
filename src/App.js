84788a3a7770dcd4b7a03c441737670662ece2a2
proywanat <proywanat@gmail.com>
2024-11-06 16:34:08 +0700

    feat: add css and assets

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Sidebar from "./components/sidebar/Sidebar";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Service from "./components/services/Service";
+import Portfolio from "./components/portfolio/Portfolio";
+import Resume from "./components/resume/Resume";
+import Contact from "./components/contact/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Sidebar />
+      <main className="main">
+        <Home />
+        <About />
+        <Service />
+        <Portfolio />
+        <Resume />
+        <Portfolio />
+        <Contact />
+      </main>
+    </>
   );
 }
 
